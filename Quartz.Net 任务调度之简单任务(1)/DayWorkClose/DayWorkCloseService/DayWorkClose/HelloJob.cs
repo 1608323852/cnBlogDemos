da90c342f6cf407ed60f0f826d7b68387c236dea
孙帅泽 <sun645430@163.com>
2019-12-23 17:08:03 +0800

    添加【Quartz.Net任务调度之简单任务(1)】

---
+++ Quartz.Net 任务调度之简单任务(1)/DayWorkClose/DayWorkCloseService/DayWorkClose/HelloJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayWorkCloseService.DayWorkClose
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Task.Run(() => {
+
+                Console.WriteLine($@"{"张翼德"}"+DateTime.Now + "");
+
+            });
+        }
+    }
+}
---
+++ Quartz.Net 任务调度之简单任务(1)/DayWorkClose/DayWorkCloseService/DayWorkCloseManager.cs
@@ -0,0 +1,55 @@
+﻿using DayWorkCloseService.DayWorkClose;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayWorkCloseService
+{
+    public class DayWorkCloseManager
+    {
+
+        public static async Task Init()
+        {
+            try
+            {
+                #region 创建单元 (时间轴/载体)
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+                IScheduler scheduler = await factory.GetScheduler();
+                await scheduler.Start();
+                #endregion
+
+                #region job
+                IJobDetail  jobDetail = JobBuilder.Create<HelloJob>()
+                    .WithDescription("this is a job")
+                  .WithIdentity("job1", "group1")
+                  .Build();
+                #endregion
+
+                #region 时间策略
+                ITrigger trigger = TriggerBuilder.Create()
+                                   .WithIdentity("trigger1", "group1")
+                                   .StartNow()
+                                   .WithSimpleSchedule(x => x
+                                   .WithIntervalInSeconds(1)
+                                   .WithRepeatCount(10000))
+                                   .Build();
+                #endregion
+
+                // Tell quartz to schedule the job using our trigger
+                await scheduler.ScheduleJob(jobDetail, trigger);
+
+               
+
+
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+        }
+    }
+}
